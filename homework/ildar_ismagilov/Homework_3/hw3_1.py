0a0a71fb438859f3ff2439724c92acab49ff01cd
Ildar Ismagilov <ildarraddo@gmail.com>
2024-12-20 20:38:22 +0300

    Homework 3, 4 tasks

---
+++ homework/ildar_ismagilov/Homework_3/hw3_1.py
@@ -0,0 +1,7 @@
+a = 5
+b = 2
+
+print('a + b =', a + b)
+print('a - b =', a - b)
+print('a * b =', a * b)
+print('a / b =', a / b)
---
+++ homework/ildar_ismagilov/Homework_3/hw3_3.py
@@ -0,0 +1,8 @@
+x = 10
+y = 12
+
+arithmetic_average = (x + y) / 2
+geometric_average = (x * y) ** 0.5
+
+print('Arithmetic average of x and y =', arithmetic_average)
+print('Geometric average of x and y =', geometric_average)
